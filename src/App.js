78fb8a77acff9c8cb12dc8b8d76b23e5712c5df1
Tiannamen <fernandonsc5@gmail.com>
2023-11-05 19:59:37 -0300

    new main layout, v2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,82 @@
-import Items from "./components/items";
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Card, CardBody, Table } from 'react-bootstrap';
+import Item from './components/item';
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div class="container">
-      <div class="Header">
-        <h1>My Game List</h1>
-      </div>
-      <div class="Playing">
-        <Items blockName="On going list" />
-      </div>
-      <div class="Wanted">
-        <Items blockName="Waiting list" />
-      </div>
-      <div class="Finished">
-        <Items blockName="Finished list" />
-      </div>
-      <div class="Add-button">
-        <Button variant="primary" size="lg">
-          Add game
-        </Button>
-      </div>
-    </div>
+    <>
+      <section className="gradient-custom-2 vh-100">
+        <Container className="py-5 h-100">
+          <Row className="d-flex justify-content-center align-items-center">
+            <Col md="12" xl="10">
+              <Card className="mask-custom">
+                <CardBody className="p-4 text-white">
+                  <div className="text-center pt-3 pb-2">
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-todo-list/check1.webp"
+                      alt="Check"
+                      width="60"
+                    />
+                    <hr></hr>
+                  </div>
+                  <Table className="text-white mb-0">
+                    <thead>
+                      <tr>
+                        <th scope="col">Game</th>
+                        <th scope="col">Complement</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <Item imagePath="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                        altText="avatar 1" gameName="The Legend of Zelda" description="Link's Awakening" gameStatus="playing"/>
+                      <Item imagePath="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                        altText="avatar 2" gameName="The Legend of Zelda" description="Breath of the Wild" gameStatus="playing"/>
+                      <Item imagePath="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                        altText="avatar 2" gameName="Super Mario Bros." description="In Wonderland!" gameStatus="playing"/>
+                    </tbody>
+                  </Table>
+                  <hr/>
+                  <div className="d-grid gap-2">
+                    <Button variant="primary" size="lg" onClick={handleShow}>
+                      Add game
+                    </Button>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add game</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Woohoo, you are reading this text in a modal!
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
---
+++ src/components/item.jsx
@@ -0,0 +1,32 @@
+import { Badge, Button } from 'react-bootstrap';
+
+function Item(props) {
+  return (
+    <tr className="fw-normal">
+      <th>
+        <img
+          src={props.imagePath}
+          alt={props.altText}
+          style={{ width: "45px", height: "auto" }}
+        />
+        <span className="ms-2">{props.gameName}</span>
+      </th>
+      <td className="align-middle">
+        <span>{props.description}</span>
+      </td>
+      <td className="align-middle">
+        <h6 className="mb-0">
+          <Badge className="mx-2" bg="danger">
+            {props.gameStatus}
+          </Badge>
+        </h6>
+      </td>
+      <td className="align-middle">
+        <Button variant="primary" size="sm">Complete</Button>{' '}
+        <Button variant="danger" size="sm">Delete</Button>
+      </td>
+    </tr>
+  );
+}
+
+export default Item;

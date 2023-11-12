3a1b509cffe65e66e0c368e777643f8e545e6ae7
Fernando Tiannamen <fernandonsc5@gmail.com>
2023-11-11 21:06:03 -0300

    Added backend

---
+++ backend/src/main/java/com/tiannamen/puctg/PuctgApplication.java
@@ -0,0 +1,13 @@
+package com.tiannamen.puctg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuctgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuctgApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tiannamen/puctg/PuctgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiannamen.puctg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuctgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

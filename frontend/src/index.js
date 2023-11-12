a7b5ca01152f1c339d8164df6fd22a1dbafffe2d
Fernando Tiannamen <fernandonsc5@gmail.com>
2023-11-11 21:44:29 -0300

    Removd unused imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div className='rootStyle'>

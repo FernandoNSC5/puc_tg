c6634c1d3cde67c4682c04cb5c04c67185cbeeca
tiannamen <fernandonsc5@gmail.com>
2023-12-02 23:27:39 -0300

    Add reqeust to freegames api

--- backend/src/main/java/com/tiannamen/puctg/controller/MainController.java
+++ backend/src/main/java/com/tiannamen/puctg/controller/MainController.java
@@ -1,11 +1,9 @@
 package com.tiannamen.puctg.controller;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import com.tiannamen.puctg.model.dto.GameDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,8 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -29,6 +30,29 @@ public class MainController {
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
     public MainController() {
+    }
+
+    @GetMapping("/freeGames")
+    @CrossOrigin
+    public ResponseEntity<String> getAllFreeGames() throws IOException {
+        URL url = new URL("https://www.freetogame.com/api/games ");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        int status = con.getResponseCode();
+        if (status == 200) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+            return ResponseEntity.ok(content.toString());
+        }
+
+        return ResponseEntity.internalServerError().body("Failed to connect to freetogame.com");
     }
 
     @GetMapping
